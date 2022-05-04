30c4806033acac2372b5029c65c9613121d5ab5a
Corban <sonnemanc@gmail.com>
2022-05-04 09:41:22 -0700

    created basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.delete_all
+Provider.delete_all
+Visit.delete_all
+
+User.create(first_name: 'John', last_name: 'Smith', username: 'BestDev', password: 'guest')
+Provider.create(first_name: 'Antony', last_name: 'Todd', title: 'Dr.')
+Visit.create(user_id: 1, provider_id: 1)
