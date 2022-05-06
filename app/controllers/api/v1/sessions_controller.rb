77ed837d1c0e1cc5b9183c4b34b30bf1eab8de53
Corban <sonnemanc@gmail.com>
2022-05-06 08:05:45 -0700

    sessions destroy

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
+
+    def create
+
+    end
+
+    def get_current_user
+
+    end
+
+    def destroy
+        session.clear
+        render json: {
+            notice: 'successfully logged out'
+        }, status: :ok
+    end
+
 end
