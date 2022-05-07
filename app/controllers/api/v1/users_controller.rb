4c5f31cfac2f21c822b2c8c06543d9420310f9c5
Corban <sonnemanc@gmail.com>
2022-05-07 11:10:25 -0700

    user create, update, destroy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,26 +10,32 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-       render json: User.create!(user_params)
+      @user = User.create(user_params)
+
+      if @user.save
+        render json @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
 
     def update
-
+        @user.update(user_params)
     end
 
     def destroy
-
+        @user.destroy
     end
 
     private
 
+#this is where I define @user for show, update, and destroy
     def set_user
       @user = User.find(params[:id])
     end
 
     def user_params
         params.permit(:email, :last_name, :first_name, :username, :password)
-
     end
 
 
