8664e3482a2732a5def667df160e8a0df75d3a90
Corban <sonnemanc@gmail.com>
2022-05-11 09:24:49 -0700

    added purpose to visit serializer user#show now sends visit info for specific patient

--- app/serializers/visit_serializer.rb
+++ app/serializers/visit_serializer.rb
@@ -1,5 +1,5 @@
 class VisitSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :provider_id
+  attributes :id, :user_id, :provider_id, :purpose
   belongs_to :author
   belongs_to :user
 end
